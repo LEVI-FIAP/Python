4b0bad9579d81e0b3709ee197477cb9361b6b565
Erick0105 <erickaxs0105@gmail.com>
2024-11-13 12:24:06 -0300

    fazendo o update

--- cods/database.py
+++ cods/database.py
@@ -18,7 +18,7 @@ class Repositorio():
     def gravar_db(self, usuario : Usuario , relatorio : Relatorio) -> bool:
         conexao = self.gerar_conexao_db()
         cursor = conexao.cursor()
-        sql_usu = """INSERT INTO usuario (id_usu, email_usu, senha_usu, consumo_men, area_desejada, nome_usu, Regiao_id_reg, Painel_id_painel) VALUES (:1, :2, :3, :4, :5, :6, :7, :8)"""
+        sql_usu = """INSERT INTO usuario (id_usu, email_usu, senha_usu, consumo_men, area_desejada, nome_usu, Regiao_id_reg, Relatorio_id_relatorio) VALUES (:1, :2, :3, :4, :5, :6, :7, :8)"""
         sql_rel = """INSERT INTO relatorio (id_relatorio, qtnd_pain, potencia_total, custo_total, economia_mensal, payback, energia_mes) VALUES (:1, :2, :3, :4, :5, :6, :7)"""
         try:
             cursor.execute(sql_usu, (usuario.id_user, usuario.email_user, usuario.senha_user, usuario.consumo_men, usuario.area_desejada, usuario.nome_usuario, usuario.regiao_fk, usuario.relatorio_fk))
@@ -69,14 +69,15 @@ class Repositorio():
         conexao = self.gerar_conexao_db()
         cursor = conexao.cursor()
         sql_user = """SELECT * FROM usuario WHERE id_usu LIKE :1"""
-        sql_end = """SELECT * FROM relatorio WHERE id_relatorio LIKE :1"""
+        sql_rel = """SELECT * FROM relatorio WHERE id_relatorio LIKE :1"""
         try:
             resultado = []
             cursor.execute(sql_user, (id_procurado_usuario,))            
             for dados in cursor:
                 resultado.append(dados)
+            id_procurado_relatorio =  cursor[0][-1]
                 
-            cursor.execute(sql_end, (id_procurado,))
+            cursor.execute(sql_rel, (id_procurado_relatorio,))
             for dados in cursor:
                 resultado.append(dados)
         except Exception as err:
@@ -86,16 +87,16 @@ class Repositorio():
         conexao.close()
         return resultado
 
-    def update_db (self, cliente : Cliente , endereco : Endereco, id_clie : int):
+    def update_db (self, usuario : Usuario , relatorio : Relatorio, id_clie : int):
         conexao = self.gerar_conexao_db()
         cursor = conexao.cursor()
-        sql_clie = """UPDATE cliente SET email_cliente = :1, nome_cliente = :2, cpf = :3, data_nascimento = TO_DATE(:4, 'YYYY-MM-DD'), senha_clie = :5, telefone_clie = :6 WHERE id_cliente = :7"""
-        sql_end = """UPDATE endereco SET cep = :1, numero =:2, cidade =:3 , rua =:4, uf = :5, complemento = :6, bairro =:7 WHERE id_cliente = :8 """
+        sql_user = """UPDATE usuario SET email_usu = :1, senha_usu = :2, consumo_men = :3, area_desejada = :4, nome_usu = :5, Regiao_id_reg = :6 WHERE id_cliente = :7"""
+        sql_end = """UPDATE relatorio SET qtnd_pain = :1, potencial_total =:2, custo_total =:3 , economia_mensal =:4, payback = :5, energia_mes = :6  WHERE id_cliente = :7"""
         try:
-            cursor.execute(sql_clie, (cliente.email_cliente, cliente.nome_cliente, cliente.cpf, cliente.data_nascimento, cliente.senha_clie, cliente.telefone_clie, id_clie))
+            cursor.execute(sql_user, (usuario.id_user, usuario.email_user, usuario.senha_user, usuario.consumo_men, usuario.area_desejada, usuario.nome_usuario, usuario.regiao_fk))
             conexao.commit()
-            
-            cursor.execute(sql_end, (endereco.cep, endereco.numero, endereco.cidade, endereco.rua, endereco.uf, endereco.complemento, endereco.bairro, id_clie))
+            # Fazer o negocio para procurar o id
+            cursor.execute(sql_end, (relatorio.qtnd_painel, relatorio.potencia_total, relatorio.custo_total, relatorio.economia_mensal, relatorio.payback, relatorio.energia_mes, relatorio.id_relatorio))
             conexao.commit()
         except Exception as err:
             print("Erro: ", err)
